c6574e1bb5f70978b502cc3aa7bcf4f37c7967f9
karine Durand <45596232+karinedurand@users.noreply.github.com>
2025-03-11 08:12:13 +0100

    Delete MAG_assembly directory

--- MAG_assembly/metabat2.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#SBATCH -p dgimi-eha
-#SBATCH --ntasks=1
-#SBATCH --cpus-per-task=16
-#SBATCH --mem=100G
-cd /lustre/durandk/Mock2/Metapasdes/
-# MetaBAT2
-metabat2 -i contigs.fasta -a coverage.txt -o bin  -t 16
